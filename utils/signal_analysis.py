41d70e5f56c3122390d533850392d433fbb41d54
Anant Gupta <anantgupta129@gmail.com>
2021-10-30 11:58:33 +0530

    model training

--- utils/signal_analysis.py
+++ utils/signal_analysis.py
@@ -12,14 +12,14 @@ def plot_power_spectrum(df, col):
         col ([str]): column name from which noise to removed
     """
     n = len(df)
-    dt = 0.25 # sampling frequenc=y
-    fhat = np.fft.fft(df[col], n) # Compute FFT
-    PSD = fhat * np.conj(fhat) / n # Power spectrum
-    freq = (1/(dt*n)) * np.arange(n) # Create X- axis of ferquencies
+    dt = 0.25  # sampling frequenc=y
+    fhat = np.fft.fft(df[col], n)  # Compute FFT
+    PSD = fhat * np.conj(fhat) / n  # Power spectrum
+    freq = (1/(dt*n)) * np.arange(n)  # Create X- axis of ferquencies
     L = np.arange(1, np.floor(n/15), dtype='int')
 
-    _, axs = plt.subplots(3,1, figsize=(25,15))
-    
+    _, axs = plt.subplots(3, 1, figsize=(25, 15))
+
     plt.sca(axs[0])
     plt.title('Original Signal (noisy)')
     plt.plot(df['seconds'], df[col], label='Noisy')
@@ -29,12 +29,12 @@ def plot_power_spectrum(df, col):
     plt.plot(freq[L], PSD[L])
     plt.xlim(freq[L[0]], freq[L[-1]])
     plt.title('Power Spectrum')
-    #plt.show()
+    # plt.show()
 
-    indices = PSD > 0.00001 # finad all frequecies larger then power spectrum
-    PSDclean = PSD * indices # zero out all other indices
-    fhat = indices * fhat # zero out all fourier coff in Y
-    ffilt = np.fft.ifft(fhat) # inverse FFT for filtered time signal
+    indices = PSD > 0.00001  # finad all frequecies larger then power spectrum
+    PSDclean = PSD * indices  # zero out all other indices
+    fhat = indices * fhat  # zero out all fourier coff in Y
+    ffilt = np.fft.ifft(fhat)  # inverse FFT for filtered time signal
 
     plt.sca(axs[2])
     plt.title('DeNoised Signal')
@@ -54,7 +54,7 @@ def f(df, x):
 
 def plot_interpolated_df(df):
 
-    _, axs = plt.subplots(2,1, figsize=(25,12))
+    _, axs = plt.subplots(2, 1, figsize=(25, 12))
     plt.sca(axs[0])
     plt.plot(df['seconds'], df['FHR'], label='Noisy')
     plt.legend()
@@ -82,25 +82,25 @@ def interpolate_df(df, col):
     flag = None
     k = len(df)
     i = 0
-   
-    while i<= k-t:
+
+    while i <= k-t:
         x = df[col][i]
-        if x>=50:
+        if x >= 50:
             pass
         else:
             count = 0
             while True:
-                if df[col][i+count]==0:
-                    count +=1
+                if df[col][i+count] == 0:
+                    count += 1
                 else:
                     flag = 'yes'
                     break
-                if count==t+1:
+                if count == t+1:
                     flag = 'no'
                     #shift = 0
                     i += count
-                    while i<=k-t:
-                        if df[col][i]==0:
+                    while i <= k-t:
+                        if df[col][i] == 0:
                             i += 1
                         else:
                             break
@@ -108,9 +108,7 @@ def interpolate_df(df, col):
                     break
             if flag == 'yes':
                 values[i] = f(df, x)
-        i +=1
-        
+        i += 1
+
     df[col+'_denoised'] = values
     return df
-
-
